d3872bb698e172489e2f87514a9ae8283b3debf7
Alvaro Zetino <Alvaro@MacBook-Pro.local>
2018-02-27 01:07:16 -0800

    initial commit

--- scripts/windchill.js
+++ scripts/windchill.js
@@ -1,29 +1,36 @@
 <!DOCTYPE HTML>
 <html lang="en-us">
 <head>
-<meta charset="utf-8">
-<title>Wind Chill</title>
-<script type="text/javascript">
+    <meta charset="utf-8">
+    <title>Windchill in Fahrenheit</title>
+    <script type="text/javascript"> 
+    /* input: user inputs temperature and wind speed
+    * Process: It runs through formula
+    * output: Gives user windchill in farenheit 
+    */
+  function doinputoutput() {
+  
+  var temp = document.getElementById("tempinputbox").value;
+  var speed = document.getElementById("speedinputbox").value;
+  var chill = windchill(temp, speed)
+  document.getElementById("output").innerHTML= chill + "°f";
+  }
+  
+  function windchill(tempF, speed) {
+  return 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF
+  *Math.pow(speed, 0.16);
+  }
+  </script>
+  </head>
+  <body>
 
-function doInputOutput() {
-var t = document.getElementById('tInputBox').value; 
-var s = document.getElementById('sInputBox').value; 
-var wchill = windChill(t, s) 
-document.getElementById ('outputDiv').innerHTML = "Wind Chill factor is " + wchill + " Fahrenheit";
-} 
-function windChill(tempF, speed)  {
-return 35.74 + (0.6215 * tempF) - (35.75 * (speed ** 0.16)) + ((0.4275 * tempF) * (speed ** 0.16));
-}
-</script>
-</head>
-<body>
-<h1>Calculate Wind Chill</h1>
-<h3>Please provide the following information:</h3><br>
-<h4>Temperature in Fº:<input type="text" value="temperature" id='tInputBox' size="12"></h4>
-<br>
-<h4>Wind Speed in MPH:<input type="text" value="wind speed" id='sInputBox' size="12"></h4>
-<br>
-<button type="button" onclick="doInputOutput()">Wind chill</button>
-<div id="outputDiv"></div>
-</body>
-</html>
+  <H1>Windchill Calculator</H1>
+  Enter Temperature: <input type="text" id='tempinputbox'>
+  <br>
+  Enter Wind Speed: <input type="text" id='speedinputbox'>
+  <br>
+  <button type="button" onclick="doinputoutput()">Wind Chill</button>
+  <div id="output">
+  </div>
+  </body>
+  </html>
