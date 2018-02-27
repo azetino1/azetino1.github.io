abb954b41f28af752902e00a6e3f52dc43647231
Alvaro Zetino <Alvaro@MacBook-Pro.local>
2018-02-27 01:20:22 -0800

    initial commit

--- scripts/windchill.js
+++ scripts/windchill.js
@@ -1,35 +1,29 @@
 <!DOCTYPE HTML>
 <html lang="en-us">
 <head>
-    <meta charset="utf-8">
-    <title>Windchill in Fahrenheit</title>
-    <script type="text/javascript"> 
-    /* input: user inputs temperature and wind speed
-    * Process: It runs through formula
-    * output: Gives user windchill in farenheit 
-    */
-  function doinputoutput() {
-  
-  var temp = document.getElementById("tempinputbox").value;
-  var speed = document.getElementById("speedinputbox").value;
-  var chill = windchill(temp, speed)
-  document.getElementById("output").innerHTML= chill + "°f";
-  }
-  
-  function windchill(tempF, speed) {
-  return 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF
-  *Math.pow(speed, 0.16);
-  }
-  </script>
-  </head>
-  <body>
+ <meta charset="utf-8">
+ <title>Wind Chill Calculator</title>
+ <script type="text/javascript">
 
-  Enter Temperature: <input type="text" id='tempinputbox'>
-  <br>
-  Enter Wind Speed: <input type="text" id='speedinputbox'>
-  <br>
-  <button type="button" onclick="doinputoutput()">click for Wind Chill</button>
-  <div id="output">
-  </div>
-  </body>
-  </html>
+ 
+ function doInputOutput() {
+ var userTemp = parseFloat(document.getElementById('tempInput').value);
+ var userSpeed = parseFloat(document.getElementById('speedInput').value);
+ var chillOut = windChill(userTemp, userSpeed);
+ document.getElementById('outputDiv').innerHTML = chillOut;
+ }
+ 
+ function windChill(tempF, speed) {
+ chillCalc = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16)
+ return chillCalc;
+ }
+ </script>
+</head>
+
+<body>
+ Temperature:<input type="text" id="tempInput"><br>
+ Wind Speed:<input type="text" id="speedInput"><br>
+ <button type="button" onclick="doInputOutput()">click for Wind Chill</button>
+ <div id="outputDiv"></div>
+</body>
+</html>
