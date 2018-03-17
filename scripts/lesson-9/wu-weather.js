79edd801ae35f9a364b787aa23c4d861a7333b75
Alvaro Zetino <Alvaro@Alvaros-MacBook-Pro.local>
2018-03-17 01:02:31 -0700

    initial commit

--- scripts/lesson-9/wu-weather.js
+++ scripts/lesson-9/wu-weather.js
@@ -14,5 +14,5 @@ var weatherInfo = JSON.parse(weatherObject.responseText);
     document.getElementById('currentWeather').innerHTML = weatherInfo.current_observation.weather;
     document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
     document.getElementById('currentWindspeed').innerHTML = weatherInfo.current_observation.wind_gust_mph;
-}
+}   document.getElementById('w_icon').src = weatherInfo.current_observation.icon_url;
     
