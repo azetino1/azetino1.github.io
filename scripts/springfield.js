ee5f49d7f60f4e0db04b1f81c1eb0e2c69990b47
Alvaro Zetino <Alvaro@Alvaros-MacBook-Pro.local>
2018-03-18 06:43:37 -0700

    initial commit

---
+++ scripts/springfield.js
@@ -0,0 +1,19 @@
+//javaScript
+
+var weatherObject = new XMLHttpRequest();
+
+weatherObject.open('GET','https://api.wunderground.com/api/c68b3124bacf9ecd/conditions/q/TN/Springfield.json',true);
+
+weatherObject.send();
+
+weatherObject.onload = function(){
+    
+var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('currentWeather').innerHTML = weatherInfo.current_observation.weather;
+    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
+    document.getElementById('w_icon').src = weatherInfo.current_observation.icon_url;
+    document.getElementById('currentWindspeed').innerHTML = weatherInfo.current_observation.wind_mph;
+    document.getElementById('currentWindchill').innerHTML = weatherInfo.current_observation.windchill_string;
+    document.getElementById('currentHigh').innerHTML = weatherInfo.forecast.txt_forecast.forecastday["0"].fcttext;
