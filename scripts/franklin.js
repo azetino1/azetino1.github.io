ab04e44b2065af65fe99641b1b0f7f41fb24da40
Alvaro Zetino <Alvaro@alvaros-mbp.ad.ccsd.net>
2018-03-21 13:07:36 -0700

    initial commit

---
+++ scripts/franklin.js
@@ -0,0 +1,23 @@
+//javaScript
+
+var weatherObject = new XMLHttpRequest();
+
+weatherObject.open('GET','https://api.wunderground.com/api/f8686be8191dbc65/conditions/forecast/q/MN/Franklin.json',true);
+
+weatherObject.send();
+
+weatherObject.onload = function(){
+    
+var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('currentWeather').innerHTML = weatherInfo.current_observation.weather;
+    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temperature_string;
+    document.getElementById('w_icon').src = weatherInfo.current_observation.icon_url;
+    document.getElementById('currentWindspeed').innerHTML = weatherInfo.current_observation.wind_mph;
+    document.getElementById('currentWindchill').innerHTML = weatherInfo.current_observation.windchill_string;
+    document.getElementById('currentHigh').innerHTML = weatherInfo.forecast.txt_forecast.forecastday["0"].fcttext;
+  
+
+}  
+    
