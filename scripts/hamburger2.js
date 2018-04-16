2fdd26d69a3caaec473f337e7d5afe4673e537f8
azetino1 <zet16001@byui.edu>
2018-04-15 21:56:07 -0700

    initial commit

--- scripts/hamburger2.js
+++ scripts/hamburger2.js
@@ -1,4 +1,4 @@
 function toggleNavMenu(){
-    //console.log(document.getElementById("primaryNav").classList);
+    //console.log(document.getElementById("dropdownMenu").classList);
     document.getElementById("dropdownMenu").classList.toggle("hide");
 }
